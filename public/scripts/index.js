291cc9d3b10c6de85a726ab81e94a2050643cccd
umnovjp <umnovjp@yahoo.com>
2025-08-08 23:43:36 -0500

    still cannot login from new refurbished laptop

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -172,16 +172,12 @@ function selectGame() {var inputVal = document.getElementById('datepicker').valu
     "method": "GET", "headers": {}
   })
     .then(function (response) {return response.json()})
-    .then(function (data2) { 
-      console.log('I am in schedule then');
+    .then(function (data2) { console.log('I am in schedule then');
       var numberOfGames = data2.gameWeek[0].games.length;
-      for (var i = 0; i < numberOfGames; i++) {
-        var gameName = document.createElement('button');
-        gameName.setAttribute('id', 'game' + i);
-        var idx = gameName.getAttribute('id');
+      for (var i = 0; i < numberOfGames; i++) { var gameName = document.createElement('button');
+        gameName.setAttribute('id', 'game' + i); var idx = gameName.getAttribute('id');
         gameName.innerHTML = 'Game ' + i + ': ' + data2.gameWeek[0].games[i].awayTeam.abbrev + ' ' + data2.gameWeek[0].games[i].homeTeam.abbrev;
-        document.getElementById('gamesPlayed').appendChild(gameName);
-        gameName.addEventListener('click', displayGameData);
+        document.getElementById('gamesPlayed').appendChild(gameName); gameName.addEventListener('click', displayGameData);
       }
       function displayGameData(event) { idx = event.currentTarget; idxString = event.currentTarget.textContent;
         idxArray = idxString.split(':'); idxNumber = idxArray[0].split(' ');
@@ -190,8 +186,7 @@ function selectGame() {var inputVal = document.getElementById('datepicker').valu
         console.log(gameId);
         // var requestURL = 'https://corsproxy.io/https://api-web.nhle.com/v1/gamecenter/' + gameId + '/play-by-play';
         var requestURL = 'https://cors-anywhere.herokuapp.com/https://api-web.nhle.com/v1/gamecenter/' + gameId + '/boxscore';
-        fetch(requestURL, {
-          "method": "GET", "headers": { }
+        fetch(requestURL, { "method": "GET", "headers": { }
         })
           .then(function (response) { return response.json()})
           .then(function (data) { console.log('I am in second then');
@@ -199,11 +194,8 @@ function selectGame() {var inputVal = document.getElementById('datepicker').valu
             var shiftsURL = 'https://cors-anywhere.herokuapp.com/https://api.nhle.com/stats/rest/en/shiftcharts?cayenneExp=gameId=' + gameId;
             fetch(shiftsURL, { "method": "GET", "headers": {} })
             .then(function (response) {return response.json()})
-            .then(function (data_shifts) {
-              // to add function here tomorrow
-              console.log ('I am in 3rd then')
+            .then(function (data_shifts) { console.log (data.playerByGameStats.homeTeam)
             })
-
           }
         );
       } // end displayGamedata
